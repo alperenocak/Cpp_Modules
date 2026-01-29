d7ad1b64663127d23dd125cc92772edf2e4c5a5f
Yusuf Alperen Ocak <yuocak@k2m05s07.42kocaeli.com.tr>
2026-01-29 13:00:52 +0300

    cpp01 ex04 completed

---
+++ try_something/find.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::string s;
+    size_t pos = 0;
+    size_t foundPos;
+    std::string new_s;
+
+    s = "selam ben yusuf alperen ocak";
+
+    while (foundPos = s.find("ben", pos) != std::string::npos)
+    {
+        
+    }
+    
+}
