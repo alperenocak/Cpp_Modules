4ff9b3db2cbc8b61a54871bc335ab3f62fb460ff
Yusuf Alperen Ocak <yuocak@k2m05s01.42kocaeli.com.tr>
2026-02-02 13:23:55 +0300

    ex00 cannonical form comp

---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuocak <yuocak@student.42kocaeli.com.tr>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-02-02 09:54:43 by yuocak            #+#    #+#             */
+/*   Updated: 2026-02-02 09:54:43 by yuocak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+int main()
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
