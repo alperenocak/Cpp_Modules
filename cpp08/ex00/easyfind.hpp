f2c9d7b47516c37d4f2d98328d304f2271e952a8
alperenocak <yusufalperenocak10@gmail.com>
2026-07-20 17:44:47 +0300

    add: easydin template

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <algorithm>
+
+class NoFound : public std::exception
+{
+public:
+    virtual const char* what() const throw(){
+        return "not found";
+    }
+};
+template<typename T>
+typename T::iterator easyfind(T &array, int number)
+{
+    typedef typename T::iterator arrayIt;
+
+    arrayIt it = std::find(array.begin(), array.end(), number);
+    if(it == array.end())
+        throw NoFound();
+    return it;
+}
+
+#endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "easyfind.hpp"
+
+
+int main()
+{
+    std::vector<int> array;
+    array.push_back(10);
+    array.push_back(20);
+    array.push_back(30);
+    easyfind(array, 20);
+}
