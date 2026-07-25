e3a2344786e087097cef720adaac4502b6631561
alperenocak <yusufalperenocak10@gmail.com>
2026-07-25 15:07:13 +0300

    feat(cpp08): add ex01 and ex02

---
+++ cpp08/ex01/Span.cpp
@@ -0,0 +1,52 @@
+#include "Span.hpp"
+
+
+Span::Span(){}
+Span::Span(unsigned int number): _maxLimit(number)
+{
+    _numbers.reserve(number);
+
+}
+Span::Span(const Span &other): _maxLimit(other._maxLimit), _numbers(other._numbers){}
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other)
+    {
+        this->_maxLimit = other._maxLimit;
+        this->_numbers = other._numbers;
+    }
+    return *this;
+}
+Span::~Span(){}
+
+int Span::shortestSpan() const
+{
+    
+    if (this->_numbers.size() < 2)
+        throw NotMoreOne();
+
+    std::vector<int> v = _numbers;
+    std::sort(v.begin(), v.end());
+    std::vector<int> diff(v.size());
+    std::adjacent_difference(v.begin(), v.end(), diff.begin());
+
+    return *std::min_element(diff.begin() + 1, diff.end());//+1 sebebi ise ilk indexte sayının kendisi var.
+}
+
+int Span::longestSpan() const
+{
+    if(this->_numbers.size() < 2)
+        throw NotMoreOne();
+    
+    return (*std::max_element(_numbers.begin(), _numbers.end()) - *std::min_element(_numbers.begin(), _numbers.end()));
+}
+
+void Span::addNumber(int number)
+{
+    if(_numbers.size() >= _maxLimit)
+    {
+        throw MaxLimit();
+    }
+    _numbers.push_back(number);
+}
+
---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Span.hpp"
+#include <iostream>
+
+int main()
+{
+    try
+    {
+        
+        Span sp = Span(5);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        return 0;
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
