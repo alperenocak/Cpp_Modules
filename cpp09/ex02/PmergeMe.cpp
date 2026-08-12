0d546057eee4720374287caaa18503767b79e504
alperenocak <yusufalperenocak10@gmail.com>
2026-08-12 17:05:16 +0300

    feat: enhance PmergeMe class with copy constructor and assignment operator, improve Jacobsthal calculation, and refine input parsing

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -4,16 +4,39 @@ PmergeMe::PmergeMe()
 {
 }
 
+PmergeMe::PmergeMe(const PmergeMe& src) : _vectorList(src._vectorList), _dequeList(src._dequeList)
+{
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& rhs)
+{
+    if (this != &rhs)
+    {
+        _vectorList = rhs._vectorList;
+        _dequeList = rhs._dequeList;
+    }
+    return *this;
+}
+
 PmergeMe::~PmergeMe()
 {
 }
 
 size_t PmergeMe::getJacobsthal(size_t n)
 {
-    if(n== 0) return;
-    if(n == 1) return;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
     
-    return getJacobsthal(n-1) + 2 * getJacobsthal(n -2);
+    size_t prev2 = 0;
+    size_t prev1 = 1;
+    size_t curr = 0;
+    for (size_t i = 2; i <= n; ++i)
+    {
+        curr = prev1 + 2 * prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
 }
 
 void PmergeMe::printVector(const std::string& prefix, const std::vector<int>& vec)
@@ -26,54 +49,64 @@ void PmergeMe::printVector(const std::string& prefix, const std::vector<int>& ve
     std::cout << std::endl;
 }
 
-
 bool PmergeMe::parseInput(int ac, char **av)
 {
     for (int i = 1; i < ac; i++)
     {
         std::string arg = av[i];
+        if (arg.empty())
+            return false;
         for (size_t j = 0; j < arg.length(); j++)
         {
-            if(!std::isdigit(arg[j]))
+            if (!std::isdigit(arg[j]) && !std::isspace(arg[j]) && arg[j] != '+')
                 return false;
         }
-        long val = std::atol(av[i]);
-        if (val <= 0 || val > 2147483647)
+        std::stringstream ss(arg);
+        std::string token;
+        while (ss >> token)
         {
-            return false;
+            for (size_t j = 0; j < token.length(); j++)
+            {
+                if (j == 0 && token[j] == '+') continue;
+                if (!std::isdigit(token[j]))
+                    return false;
+            }
+            long val = std::atol(token.c_str());
+            if (val <= 0 || val > 2147483647)
+                return false;
+            _vectorList.push_back(static_cast<int>(val));
+            _dequeList.push_back(static_cast<int>(val));
         }
-        
-        _vectorList.push_back(static_cast<int>(val));
-        _dequeList.push_back(static_cast<int>(val));
     }
-    return true;
+    return !_vectorList.empty();
 }
 
 void PmergeMe::run(int ac, char **av)
 {
-    if(!parseInput(ac, av))
+    if (!parseInput(ac, av))
     {
-        std::cerr<< "Error"<<std::endl;
+        std::cerr << "Error" << std::endl;
         return;
     }
 
     printVector("Before: ", _vectorList);
-    
+
     clock_t startVec = clock();
     fordJohnsonAlgorithm(_vectorList);
-    clock_t endVec  = clock();
+    clock_t endVec = clock();
 
-    
     clock_t startDeq = clock();
     fordJohnsonAlgorithm(_dequeList);
     clock_t endDeq = clock();
 
-    printVector("After: ", _vectorList);
+    printVector("After:  ", _vectorList);
 
     double timeVec = static_cast<double>(endVec - startVec) / CLOCKS_PER_SEC * 1000000;
     double timeDeq = static_cast<double>(endDeq - startDeq) / CLOCKS_PER_SEC * 1000000;
+
     std::cout << "Time to process a range of " << _vectorList.size()
               << " elements with std::vector : " << timeVec << " us" << std::endl;
     std::cout << "Time to process a range of " << _dequeList.size()
               << " elements with std::deque : " << timeDeq << " us" << std::endl;
 }
+
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -7,39 +7,118 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <sstream>
+#include <cctype>
 
 class PmergeMe
 {
 private:
-
     std::vector<int>    _vectorList;
     std::deque<int>     _dequeList;
 
-    bool         parseInput(int ac, char **av);
+    bool                parseInput(int ac, char **av);
     static size_t       getJacobsthal(size_t n);
     static void         printVector(const std::string& prefix, const std::vector<int>& vec);
 
 public:
     PmergeMe();
+    PmergeMe(const PmergeMe& src);
+    PmergeMe& operator=(const PmergeMe& rhs);
     ~PmergeMe();
 
     template <typename Container>
-    void fordJohnsonAlgorithm(Container& container)
+    static void fordJohnsonAlgorithm(Container& container)
     {
-        container.size();
-        if(container.size() < 1) return;
+        if (container.size() <= 1)
+            return;
 
         typedef typename Container::value_type ValueType;
         typedef std::pair<ValueType, ValueType> PairType;
         typedef std::vector<PairType> PairContainer;
 
-        
-        PairContainer pairs;
-        int 
-    }
-    
-    void run(int ac, char** av);
+        bool hasStraggler = (container.size() % 2 != 0);
+        ValueType straggler;
+        if (hasStraggler)
+        {
+            straggler = container.back();
+            container.pop_back();
+        }
 
+        PairContainer pairs;
+        Container mainChain;
+        for (size_t i = 0; i < container.size(); i += 2)
+        {
+            if (container[i] > container[i + 1])
+            {
+                pairs.push_back(std::make_pair(container[i], container[i + 1]));
+                mainChain.push_back(container[i]);
+            }
+            else
+            {
+                pairs.push_back(std::make_pair(container[i + 1], container[i]));
+                mainChain.push_back(container[i + 1]);
+            }
+        }
+
+        fordJohnsonAlgorithm(mainChain);
+
+        PairContainer sortedPairs;
+        for (typename Container::iterator it = mainChain.begin(); it != mainChain.end(); ++it)
+        {
+            for (typename PairContainer::iterator pit = pairs.begin(); pit != pairs.end(); ++pit)
+            {
+                if (pit->first == *it)
+                {
+                    sortedPairs.push_back(*pit);
+                    pairs.erase(pit);
+                    break;
+                }
+            }
+        }
+        pairs = sortedPairs;
+
+        Container S;
+        S.push_back(pairs[0].second);
+        for (size_t i = 0; i < pairs.size(); ++i)
+        {
+            S.push_back(pairs[i].first);
+        }
+
+        size_t lastJacob = 1;
+        size_t jacobIndex = 2;
+        size_t P = pairs.size();
+
+        while (lastJacob < P)
+        {
+            size_t nextJacob = getJacobsthal(jacobIndex);
+            size_t target = std::min(nextJacob, P);
+
+            for (size_t i = target; i > lastJacob; --i)
+            {
+                ValueType elementToInsert = pairs[i - 1].second;
+                ValueType correspondingMax = pairs[i - 1].first;
+
+                typename Container::iterator bound = std::lower_bound(S.begin(), S.end(), correspondingMax);
+                typename Container::iterator pos = std::lower_bound(S.begin(), bound, elementToInsert);
+                S.insert(pos, elementToInsert);
+            }
+
+            lastJacob = nextJacob;
+            jacobIndex++;
+        }
+
+        if (hasStraggler)
+        {
+            typename Container::iterator pos = std::lower_bound(S.begin(), S.end(), straggler);
+            S.insert(pos, straggler);
+        }
+
+        container = S;
+    }
+
+    void run(int ac, char** av);
 };
 
 #endif
+
+
